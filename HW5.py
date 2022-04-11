fd6b76fe99f76e0749f78dc2776889dfab2b0e22
Tyler Sanbar <tylersanbar@gmail.com>
2022-04-11 17:04:51 -0500

    Q3 is done, refactored exercise into method

--- HW5.py
+++ HW5.py
@@ -60,57 +60,78 @@ def printMSE(params, MSE):
 def printBestMSE(params, MSE, bestIndex):
     print("Best - Lambda1:",params[bestIndex][0],"Lambda2:",params[bestIndex][1],"MSE:",MSE[bestIndex])
 
+def getXY(data, num_features):
+    X = data[:,0:num_features-1]
+    y = data[:,num_features]
+    return X, y
+
+def exercise(training_data, validation_data, testing_data, num_features):
+    #alpha * l1_ratio
+    lambda1 = [0,.000001,.00001,.0001,.001,.01,.1,1]
+    #alpha * (1-l1_ratio)
+    lambda2 = [0,.000001,.00001,.0001,.001,.01,.1,1]
+
+    ##Get valid a_l1 values
+    params = list(itertools.product(lambda1, lambda2))
+    a_l1 = lambdas2alpha(params)
+
+    #a) Fit an elastic net model to the training data with each possible combination of the following L1 and L2 regularization weights.
+    X, y = getXY(training_data, num_features)
+    training_Models = trainModels(X, y, a_l1)
+
+    #b) For each model trained in step (a), make a prediction for each training example, using the
+    # predict method for sklearn.linear_model.ElasticNet and calculate the mean squared
+    # error (MSE) on the training examples.
+    print("---Training Data---")
+    training_bestIndex, training_MSE, training_h = bestMSE(X, y, training_Models)
+    printMSE(params, training_MSE)
+
+    #c) Make a prediction for each validation example
+    # and calculate the mean squared error on the validation examples
+    print("---Validation Data---")
+    X, y = getXY(validation_data, num_features)
+
+    validation_Models = trainModels(X, y, a_l1)
+    validation_bestIndex, validation_MSE, validation_h = bestMSE(X, y, validation_Models)
+    printMSE(params, validation_MSE)  
+
+    #d) Which model (i.e., pair of λ1 and λ2) performed best on the training data?
+    print("---Best MSE---")
+    print("Training Data:")
+    printBestMSE(params, training_MSE, training_bestIndex)
+    #Which model performed best on the validation data?
+    print("Validation Data:")
+    printBestMSE(params, validation_MSE, validation_bestIndex)
+
+    #e)Find the best hyperparameter set (pair of λ1 and λ2) on the validation data. Train a model with
+    #the same λ1 and λ2 on both the training and validation data. 
+    #Using this model, make predictions for each testing example and calculate the mean squared error on the test examples.
+
+    combined_data = np.append(training_data, validation_data, axis = 0)
+    X, y = getXY(combined_data, num_features)
+
+    best_a_l1 = [a_l1[validation_bestIndex]]
+    combined_model = trainModels(X, y, best_a_l1)
+    
+    #Testing Data
+    X, y = getXY(testing_data, num_features)
+    testing_bestIndex, testing_MSE, testing_h = bestMSE(X, y, combined_model)
+    print("---Testing Data---")
+    print("MSE:", testing_MSE[0])
+
+#Exercise 2
+
 #Get data from CSVs
-training_data = np.loadtxt("train.csv",skiprows=1,delimiter=",")
-validation_data = np.loadtxt("validate.csv",skiprows=1,delimiter=",")
-testing_data = np.loadtxt("test.csv",skiprows=1,delimiter=",")
+sc_training_data = np.loadtxt("train.csv",skiprows=1,delimiter=",")
+sc_validation_data = np.loadtxt("validate.csv",skiprows=1,delimiter=",")
+sc_testing_data = np.loadtxt("test.csv",skiprows=1,delimiter=",")
 
-#alpha * l1_ratio
-lambda1 = [0,.000001,.00001,.0001,.001,.01,.1,1]
-#alpha * (1-l1_ratio)
-lambda2 = [0,.000001,.00001,.0001,.001,.01,.1,1]
+exercise(sc_training_data,sc_validation_data,sc_testing_data, 81)
+#Exercise 3
 
-##Get valid a_l1 values
-params = list(itertools.product(lambda1, lambda2))
-a_l1 = lambdas2alpha(params)
+#Get data from CSVs
+stab_training_data = np.loadtxt("UCITraining.csv",skiprows=1,delimiter=",")
+stab_validation_data = np.loadtxt("UCIValidation.csv",skiprows=1,delimiter=",")
+stab_testing_data = np.loadtxt("UCITesting.csv",skiprows=1,delimiter=",")
 
-#a) Fit an elastic net model to the training data with each possible combination of the following L1 and L2 regularization weights.
-X = training_data[:,0:80]
-y = training_data[:,81]
-training_Models = trainModels(X, y, a_l1)
-
-#b) For each model trained in step (a), make a prediction for each training example, using the
-# predict method for sklearn.linear_model.ElasticNet and calculate the mean squared
-# error (MSE) on the training examples.
-print("---Training Data---")
-training_bestIndex, training_MSE, training_h = bestMSE(X, y, training_Models)
-printMSE(params, training_MSE)
-
-#c) Make a prediction for each validation example
-# and calculate the mean squared error on the validation examples
-print("---Validation Data---")
-X = validation_data[:,0:80]
-y = validation_data[:,81]
-
-validation_Models = trainModels(X, y, a_l1)
-validation_bestIndex, validation_MSE, validation_h = bestMSE(X, y, validation_Models)
-printMSE(params, validation_MSE)  
-
-#d) Which model (i.e., pair of λ1 and λ2) performed best on the training data?
-printBestMSE(params, training_MSE, training_bestIndex)
-#Which model performed best on the validation data?
-printBestMSE(params, validation_MSE, validation_bestIndex)
-
-#e)Find the best hyperparameter set (pair of λ1 and λ2) on the validation data. Train a model with
-#the same λ1 and λ2 on both the training and validation data. 
-#Using this model, make predictions for each testing example and calculate the mean squared error on the test examples.
-
-X = np.append(training_data[:,0:80], validation_data[:,0:80], axis=0)
-y = np.append(training_data[:,81], validation_data[:,81], axis=0)
-
-best_a_l1 = [a_l1[validation_bestIndex]]
-combined_model = trainModels(X, y, best_a_l1)
-X = testing_data[:,0:80]
-y = testing_data[:,81]
-testing_bestIndex, testing_MSE, testing_h = bestMSE(X, y, combined_model)
-print("Testing Data MSE", testing_MSE[0])
+exercise(stab_training_data,stab_validation_data,stab_testing_data, 11)
